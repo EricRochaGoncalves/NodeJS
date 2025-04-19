0806adc9ea9bdc7a95124b64f33c41f7a6d9da91
Eric Gonçalves <166281170+EricRochaGoncalves@users.noreply.github.com>
2025-04-19 15:35:20 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,35 @@
+const { Types } = require('mysql2')
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('sistemadecadastro', 'root', 'Y@sp2006',{
+    host: 'localhost',
+    dialect: 'mysql'
+
+})
+
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+    
+
+})
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+    
+
+})
+
